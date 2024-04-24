c3c30780bae9428729e6551dac00a2f4de1dda3f
khethomngomz <kmngomezulu82@gmail.com>
2024-04-24 17:16:53 +0200

    initial commit

---
+++ src/app/maps/fullscreenmap/fullscreenmap.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit } from '@angular/core';
+
+declare var google:any;
+
+@Component({
+    selector: 'fullscreen-map-cmp',
+    templateUrl: 'fullscreenmap.component.html'
+})
+
+export class FullScreenMapsComponent implements OnInit{
+    ngOnInit(){
+	}
+}
---
+++ src/app/maps/googlemaps/googlemaps.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var google: any;
+declare var $: any;
+
+@Component({
+    selector: 'vector-maps-cmp',
+    templateUrl: './googlemaps.component.html'
+})
+
+export class GoogleMapsComponent implements OnInit{
+
+    ngOnInit(){
+    
+
+    }
+}
